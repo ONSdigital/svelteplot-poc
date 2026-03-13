251a8fb77bc0c2a31590fb3295f6c502d166bce2
Sam Bennett <sam.bennett@ons.gov.uk>
2026-03-13 19:00:29 +0000

    Merge fixes

--- onssvelteplot/src/charts/SmallMultiple.svelte
+++ onssvelteplot/src/charts/SmallMultiple.svelte
@@ -76,25 +76,6 @@
         return dataArr
     })
 
-
-    let domainX = $derived(getContinuousDomain({
-        data: allData,
-        variant: props.variant ? props.variant : 'simple',
-        categoryKey: props.yKey ? props.yKey : 'y',
-        valueKey: props.xKey ? props.xKey : 'x',
-        xDomain: props.xDomain
-    }))
-    let allData = $derived.by(() => {
-        let dataArr = []
-        let keys = Object.keys(data)
-        keys.forEach((key) => {
-            data[key].forEach((d) => {
-                dataArr.push(d)
-            })
-        })
-        return dataArr
-    })
-
     let domainX = $derived(getContinuousDomain({
         data: allData,
         variant: props.variant ? props.variant : 'simple',
