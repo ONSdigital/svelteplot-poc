ab4b37c4db8fa7399e071d6ec5a0f47342f9921d
Sam Bennett <sam.bennett@ons.gov.uk>
2026-03-27 10:58:57 +0000

    add DirectLabels and endPointMarkers

--- onssvelteplot/src/charts/LineChart.svelte
+++ onssvelteplot/src/charts/LineChart.svelte
@@ -1,6 +1,6 @@
     <script>
     
-    import { Plot, AxisY, Line } from 'svelteplot';
+    import { Plot, AxisY, Line, Dot, Text } from 'svelteplot';
     import { format } from "d3-format";
     import { timeParse, timeFormat} from "d3-time-format"
     import * as d3 from 'd3';
@@ -37,18 +37,15 @@
         xDomain,
         xFormat = ".0f",
         xFormatDate,
-        xAxisTicks,
         yDomain = "auto",
+        yAxisTicks,
         yFormat = ",.0f",
-        zFormat,
-        zFormatDate,
-		ySort,
-        zSortKey, 
         dataLabels,
+        addEndMarkers = true,
+        drawLegend,
         tooltip,
         height,
         aspectRatio = [16,9],
-        hover = false,
         margin = {top: 0, bottom: 0, right: 20}, 
         colours = defaultColours[variant],
         children
@@ -70,8 +67,6 @@
     let domainX = $derived(xDomain ? xDomain : getCategoricalDomain({
         data: data, 
         variant: variant, 
-        sort: null, 
-        sortKey: zSortKey, 
         valueKey: yKey, 
         categoryKey: xKey, 
         groupKey: zKey
@@ -81,12 +76,13 @@
 
     let categories = $derived(zKey ? [...new Set(data.map((d) => d[zKey]))] : null)
 
+    let marginRight = $derived(!drawLegend && zKey ? getAxisMargin({domain: categories}) : margin.right)
 
 </script>
 
 <Plot
     marginLeft={yAxisMargin}
-    marginRight={margin.right ? margin.right : null}
+    marginRight={marginRight}
     marginTop={margin.top ? margin.top : null}
     marginBottom={margin.bottom ? margin.bottom : null}
     height = {chartHeight} 
@@ -101,10 +97,33 @@
         label: yAxisLabel ? yAxisLabel : "",
         tickFormat: (d) => yFormat ? format(yFormat)(d) : d,
     }}>
+
     <Line 
         data={data} 
         x="x" 
         y="y" 
         strokeWidth={3} 
         stroke={(d) => categories ? colours[categories.indexOf(d[zKey])] : colours[0]} />
+
+    {#if addEndMarkers || !drawLegend}
+        <Dot
+            data={data.filter((d) => d[xKey] == domainX[domainX.length-1])}
+            x={xKey}
+            y={yKey}
+            r={4}
+            fill={(d) => categories ? colours[categories.indexOf(d[zKey])] : colours[0]}
+        />
+        {#if zKey}
+        <Text
+            data={data.filter((d) => d[xKey] == domainX[domainX.length-1])}
+            x={xKey} 
+            y={yKey}
+            dx={10}
+            text={zKey}
+            textClass="dataLabel"
+            textAnchor="start"
+            fill={(d) => categories ? colours[categories.indexOf(d[zKey])] : colours[0]}
+        />
+        {/if}
+    {/if}
 </Plot>
