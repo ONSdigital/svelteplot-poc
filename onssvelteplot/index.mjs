fce52ba74e7bc5ad6aaa3a31cc01e545b990772c
Sam Bennett <sam.bennett@ons.gov.uk>
2026-04-02 09:46:35 +0100

    Cleanup of onssvelteplot files

--- onssvelteplot/index.mjs
+++ onssvelteplot/index.mjs
@@ -1,3 +1,6 @@
 // CHARTS
 export { default as Chart } from './src/charts/Chart.svelte';
 export { default as BarChart } from './src/charts/BarChart.svelte';
+export { default as LineChart } from './src/charts/LineChart.svelte';
+export { default as DotChart } from './src/charts/DotChart.svelte';
+export { default as Beeswarm } from './src/charts/Beeswarm.svelte';
